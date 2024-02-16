08f560dc6fff8e4edcccbc0b070415e716360a36
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-02-16 15:31:34 -0800

    Update Descope java sdk version and add SSO functions

--- server/src/main/java/com/descope/java_sample_app/JavaSampleAppApplication.java
+++ server/src/main/java/com/descope/java_sample_app/JavaSampleAppApplication.java
@@ -1,9 +1,18 @@
 package com.descope.java_sample_app;
+
 import com.descope.client.*;
+import com.descope.client.Config.ConfigBuilder;
 import com.descope.exception.DescopeException;
+import com.descope.model.auth.AuthenticationInfo;
 import com.descope.model.jwt.Token;
+import com.descope.model.magiclink.LoginOptions;
+import com.descope.model.sso.SSOOIDCSettings;
+import com.descope.model.sso.SSOTenantSettingsResponse;
 import com.descope.sdk.auth.*;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,11 +30,17 @@ public class JavaSampleAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSampleAppApplication.class, args);
 	}
+
 	@Value("${descope.project.id}")
 	private String descopeProjectId;
 
+	@Value("${descope.management.key}")
+	private String descopeManagementKey;
+
+	DescopeClient descopeClient = new DescopeClient(
+			Config.builder().projectId(descopeProjectId).managementKey(descopeManagementKey).build());
+
 	public void validateSession(String sessionToken) throws DescopeException {
-		var descopeClient = new DescopeClient(Config.builder().projectId(descopeProjectId).build());
 		AuthenticationService as = descopeClient.getAuthenticationServices().getAuthService();
 		Token t = as.validateSessionWithToken(sessionToken);
 	}
@@ -60,4 +75,28 @@ public class JavaSampleAppApplication {
 			return new ResponseEntity<>("Error getting authorization header", HttpStatus.UNAUTHORIZED);
 		}
 	}
+
+	public void configureOIDCSettings(String tenantId, SSOOIDCSettings ssoOIDCSettings, List<String> domains)
+			throws DescopeException {
+		descopeClient.getManagementServices().getSsoService().configureOIDCSettings(tenantId, ssoOIDCSettings, domains);
+	}
+
+	public void loadSettings(String tenantId) throws DescopeException {
+		SSOTenantSettingsResponse res = descopeClient.getManagementServices().getSsoService().loadSettings(tenantId);
+	}
+
+	public void deleteOIDCSettings(String tenantId) throws DescopeException {
+		descopeClient.getManagementServices().getSsoService().deleteSettings(tenantId);
+	}
+
+	public void startSSO(String tenantId, String redirectUrl, String prompt, LoginOptions loginOptions)
+			throws DescopeException {
+		descopeClient.getAuthenticationServices().getSsoServiceProvider().start(tenantId, redirectUrl, prompt,
+				loginOptions);
+	}
+
+	public void exchangeToken(String code) throws DescopeException {
+		AuthenticationInfo ai = descopeClient.getAuthenticationServices().getSsoServiceProvider().exchangeToken(code);
+	}
+
 }
